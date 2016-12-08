d34bf1b16a153cc6680b21e8bfba834c9a57f381
michelemei <michele.mei@gmail.com>
2016-12-08 22:39:24 +0100

    rename and comments

--- src/g2d/Point.cpp
+++ src/g2d/Point.cpp
@@ -71,10 +71,10 @@ double Point::Distance(const Line& line) const
 		return dbl_abs(X - line.GetStartPoint().X);
 	}
 
-	// determinazione dei coefficienti m e q (sarebbe bello che siano gia' propri della linea)
 	double num = line.GetEndPoint().X - line.GetStartPoint().X;
 	double m = (line.GetEndPoint().Y - line.GetStartPoint().Y) / num;
-	double q = (line.GetEndPoint().X * line.GetStartPoint().Y - line.GetStartPoint().X * line.GetEndPoint().Y) / num;
+	double q = (line.GetEndPoint().X * line.GetStartPoint().Y -
+	            line.GetStartPoint().X * line.GetEndPoint().Y) / num;
 	return dbl_abs(Y - m * X - q) / sqrt(1 + m * m);
 }
 
